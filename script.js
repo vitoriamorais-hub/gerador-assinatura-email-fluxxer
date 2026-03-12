6b4c7593346c83785b27766a5d1dd1d490542713
vitoriamorais-hub <vitoria.morais@kikker.com.br>
2026-03-12 11:58:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,83 @@
-
 function gerar(){
 
 let nome = document.getElementById("nome").value
 let cargo = document.getElementById("cargo").value
 let email = document.getElementById("email").value
 let telefone = document.getElementById("telefone").value
-let site = document.getElementById("site").value
+
+let fotoInput = document.getElementById("foto")
+let logoInput = document.getElementById("logo")
+
+let foto = ""
+let logo = ""
+
+if(fotoInput.files.length > 0){
+let readerFoto = new FileReader()
+readerFoto.onload = function(e){
+foto = e.target.result
+gerarLogo()
+}
+readerFoto.readAsDataURL(fotoInput.files[0])
+}else{
+gerarLogo()
+}
+
+function gerarLogo(){
+
+if(logoInput.files.length > 0){
+
+let readerLogo = new FileReader()
+
+readerLogo.onload = function(e){
+
+logo = e.target.result
+
+montarAssinatura(nome,cargo,email,telefone,site,foto,logo)
+
+}
+
+readerLogo.readAsDataURL(logoInput.files[0])
+
+}else{
+
+montarAssinatura(nome,cargo,email,telefone,site,foto,"")
+
+}
+
+}
+
+}
+
+function montarAssinatura(nome,cargo,email,telefone,site,foto,logo){
 
 let assinatura = `
 <table style="font-family:Arial;line-height:1.6">
-<tr>
-<td>
 
-<strong style="font-size:16px">${nome}</strong><br>
-<span style="color:#666">${cargo}</span><br><br>
+<tr>
+
+<td style="padding-right:15px">
+
+${foto ? `<img src="${foto}" width="80" style="border-radius:50%">` : ""}
+
+</td>
+
+<td style="border-left:3px solid #75529D;padding-left:15px">
+
+<strong style="font-size:16px;color:#75529D">${nome}</strong><br>
+<span style="color:#555">${cargo}</span><br><br>
 
 📧 ${email}<br>
 📞 ${telefone}<br>
 🌐 ${site}
 
+<br><br>
+
+${logo ? `<img src="${logo}" width="120">` : ""}
+
 </td>
+
 </tr>
+
 </table>
 `
 
@@ -40,6 +97,6 @@ window.getSelection().addRange(range)
 
 document.execCommand("copy")
 
-alert("Assinatura copiada! Agora cole no Gmail ou Outlook.")
+alert("Assinatura copiada!")
 
 }
